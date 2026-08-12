6d2d48090818170ebf881535229af32e9ccafcb3
George Marcone | Senior Full Stack Engineer | .NET | React | Node.js | Azure | Docker | SQL Server | Software Architecture | Scalable Systems <gmarcone@gmail.com>
2026-08-12 14:27:21 -0300

    Melhora metadados de autoria do portfólio

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ export const metadata: Metadata = {
   title: "George Marcone | Senior Software Engineer & Software Architect",
   description:
     "Portfólio de George Marcone: engenharia de software, arquitetura, liderança técnica, cloud, IA aplicada e produtos SaaS, incluindo o HemoDinks.",
+  authors: [{ name: "George Marcone" }],
+  creator: "George Marcone",
   icons: {
     icon: "/favicon.svg",
     shortcut: "/favicon.svg",
